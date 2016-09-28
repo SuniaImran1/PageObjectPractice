09cdfd9f4a00c56fd2c126dcdaae39cdbc7b1b98
SImran <SImran@ND27645.ordsvy.gov.uk>
2016-09-28 13:14:22 +0100

    Java

--- cucumberjvm-selenium-page-object/src/test/java/Java_Practice/Arrayspractice.java
+++ cucumberjvm-selenium-page-object/src/test/java/Java_Practice/Arrayspractice.java
@@ -4,23 +4,20 @@ public class Arrayspractice {
 
 	public static void main(String[] args) {
 
-//display all the elements of the list
-		double[] mylist = {1,2,3,4,5,6,7,8};
-	
-		/*	for (int i = 0; i< mylist.length; i++){
-			System.out.println("List is " + mylist[i]);
-			}*/
-		
-		//or can be done using another method
-		
-		for (double element: mylist){
+		// display all the elements of the list
+		double[] mylist = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+		/*
+		 * for (int i = 0; i< mylist.length; i++){ System.out.println("List is "
+		 * + mylist[i]); }
+		 */
+
+		// or can be done using another method
+
+		for (double element : mylist) {
 			System.out.println(element);
 		}
-					
-	}
-		
-		
-		
+
 	}
 
-
+}
